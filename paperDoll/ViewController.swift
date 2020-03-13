06840a39d8425dd0ddc5a4407b01594ece81a42f
Lucas <wayne97023@gmail.com>
2020-03-14 02:30:17 +0800

    Initial Commit

---
+++ paperDoll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  paperDoll
+//
+//  Created by 林奇杰 on 2020/3/14.
+//  Copyright © 2020 林奇杰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
